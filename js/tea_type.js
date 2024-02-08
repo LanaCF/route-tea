2021573c19cf66f058250031f863db3e39a3d951
Uliana Boiko <clubfate@gmail.com>
2024-02-09 01:32:46 +0200

    Start

---
+++ js/tea_type.js
@@ -0,0 +1,6 @@
+const teaType = [
+    { id: 1, : '', component: homePage, title: 'Home' },
+    { path: 'about', component: aboutPage, title: 'About' },
+    { path: 'catalog', component: catalogsPage, title: 'Catalog' },
+    { path: '**', component: notFoundPage, title: '' }
+];
